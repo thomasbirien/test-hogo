06d11c0678e1af0383ae62d192637796cbdc255d
thomasbirien <thomasbirien@gmail.com>
2020-02-17 18:09:58 +0100

    added update / edit and destroy

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,8 +1,8 @@
 class Insurance < ApplicationRecord
-  has_one :insurance_immovable
+  has_one :insurance_immovable, dependent: :destroy
   accepts_nested_attributes_for :insurance_immovable
-  has_one :insurance_agriculture
+  has_one :insurance_agriculture, dependent: :destroy
   accepts_nested_attributes_for :insurance_agriculture
-  has_one :insurance_automotive
+  has_one :insurance_automotive, dependent: :destroy
   accepts_nested_attributes_for :insurance_automotive
 end
